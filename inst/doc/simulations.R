b8569a7836107205a86a39c5acef3e711104bcd0
Kristoffer Magnusson <1629860+rpsychologist@users.noreply.github.com>
2017-09-08 18:59:15 +0200

    keep docs

---
+++ inst/doc/simulations.R
@@ -0,0 +1,92 @@
+## ---- echo=FALSE, warning=FALSE, message=FALSE---------------------------
+library(powerlmm)
+knitr::opts_chunk$set(eval = FALSE)
+
+## ------------------------------------------------------------------------
+#  p <- study_parameters(n1 = 11,
+#                        n2 = 10,
+#                        n3 = 6,
+#                        fixed_intercept = 37,
+#                        fixed_slope = -0.64,
+#                        sigma_subject_intercept = 2.8,
+#                        sigma_subject_slope = 0.4,
+#                        sigma_cluster_intercept = 0,
+#                        cor_subject = -0.2,
+#                        icc_slope = 0.05,
+#                        sigma_error = 2.6,
+#                        dropout = dropout_weibull(proportion = 0.3,
+#                                                  rate = 1/2),
+#                        partially_nested = TRUE,
+#                        cohend = -0.8)
+#  
+#  p
+
+## ------------------------------------------------------------------------
+#  x <- get_power(p)
+#  get_monte_carlo_se(x, nsim = c(1000, 2000, 5000))
+
+## ------------------------------------------------------------------------
+#  f <- "y ~ treatment * time + (1 + time | subject) + (0 + treatment:time | cluster)"
+#  
+#  res <- simulate(object = p,
+#                  nsim = 5000,
+#                  formula = f,
+#                  satterthwaite = TRUE,
+#                  progress = TRUE,
+#                  cores = 1,
+#                  save = FALSE)
+#  
+#  summary(res)
+
+## ------------------------------------------------------------------------
+#  p <- update(p, cohend = 0)
+#  
+#  f <- list("correct" = "y ~ treatment * time + (1 + time | subject) + (0 + treatment:time | cluster)",
+#            "wrong" = "y ~ treatment * time + (1 + time | subject)")
+#  
+#  res <- simulate(object = p,
+#                  nsim = 5000,
+#                  formula = f,
+#                  satterthwaite = TRUE,
+#                  progress = TRUE,
+#                  cores = 1,
+#                  save = FALSE)
+#  
+#  summary(res)
+
+## ------------------------------------------------------------------------
+#  p <- study_parameters(n1 = 11,
+#                        n2 = 10,
+#                        n3 = c(6, 12),
+#                        fixed_intercept = 37,
+#                        fixed_slope = -0.64,
+#                        sigma_subject_intercept = 2.8,
+#                        sigma_subject_slope = 0.4,
+#                        sigma_cluster_intercept = 0,
+#                        cor_subject = -0.2,
+#                        icc_slope = c(0.05, 0.1),
+#                        sigma_error = 2.6,
+#                        dropout = dropout_weibull(proportion = 0.3,
+#                                                  rate = 1/2),
+#                        partially_nested = TRUE,
+#                        cohend = -c(0.5, 0.8))
+#  
+#  
+#  f <- "y ~ treatment * time + (1 + time | subject) + (0 + treatment:time | cluster)"
+#  
+#  res <- simulate(object = p,
+#                  nsim = 5000,
+#                  formula = f,
+#                  satterthwaite = TRUE,
+#                  progress = TRUE,
+#                  cores = 1,
+#                  save = FALSE)
+
+## ------------------------------------------------------------------------
+#  # Summarize 'time:treatment' results
+#  summary(res, para = "time:treatment", type = "fixed", model = "correct")
+#  
+#  # Summarize cluster-level random slope
+#  summary(res, para = "cluster_slope", type = "random", model = "correct")
+#  
+
---
+++ inst/doc/three-level.R
@@ -0,0 +1,172 @@
+## ---- echo=FALSE, warning=FALSE, message=FALSE---------------------------
+library(powerlmm)
+
+## ------------------------------------------------------------------------
+p <- study_parameters(n1 = 11,
+                      n2 = 10,
+                      n3 = 4,
+                      icc_pre_subject = 0.5,
+                      icc_pre_cluster = 0,
+                      icc_slope = 0.05,
+                      var_ratio = 0.02,
+                      cohend = -0.8)
+p
+
+## ------------------------------------------------------------------------
+get_power(p)
+
+## ------------------------------------------------------------------------
+get_VPC(p)
+
+## ------------------------------------------------------------------------
+get_correlation_matrix(p)
+
+## ------------------------------------------------------------------------
+get_sds(p)
+
+## ---- fig.width = 8, message = FALSE, warning = FALSE--------------------
+library(ggplot2)
+x <- get_power_table(p, n2 = 5:20, n3 = c(4, 6, 8, 12), icc_slope = c(0.01, 0.05, 0.1))
+plot(x) + scale_x_continuous(breaks = seq(20, 240, length.out = 5))
+
+## ------------------------------------------------------------------------
+p <- study_parameters(n1 = 11,
+                      n2 = 10,
+                      n3 = per_treatment(control = 2, 
+                                         treatment = 10),
+                      icc_pre_subject = 0.5,
+                      icc_pre_cluster = 0,
+                      icc_slope = 0.05,
+                      var_ratio = 0.02,
+                      cohend = -0.8)
+p
+
+## ------------------------------------------------------------------------
+p <- study_parameters(n1 = 11,
+                      n2 = per_treatment(control = 10,
+                                         treatment = 2),
+                      n3 = per_treatment(control = 2, 
+                                         treatment = 10),
+                      icc_pre_subject = 0.5,
+                      icc_pre_cluster = 0,
+                      icc_slope = 0.05,
+                      var_ratio = 0.02,
+                      cohend = -0.8)
+p
+
+## ------------------------------------------------------------------------
+p <- study_parameters(n1 = 11,
+                      n2 = unequal_clusters(2, 5, 10, 30),
+                      icc_pre_subject = 0.5,
+                      icc_pre_cluster = 0,
+                      icc_slope = 0.05,
+                      var_ratio = 0.02,
+                      cohend = -0.8)
+p
+
+## ------------------------------------------------------------------------
+
+n2 <- per_treatment(control = unequal_clusters(5, 10, 15),
+                    treatment = unequal_clusters(2, 3, 5, 5, 10, 15, 25))
+
+p <- study_parameters(n1 = 11,
+                      n2 = n2,
+                      icc_pre_subject = 0.5,
+                      icc_pre_cluster = 0,
+                      icc_slope = 0.05,
+                      var_ratio = 0.02,
+                      cohend = -0.8)
+p
+
+## ------------------------------------------------------------------------
+p <- study_parameters(n1 = 11,
+                      n2 = unequal_clusters(2, 5, 10, 30),
+                      icc_pre_subject = 0.5,
+                      icc_pre_cluster = 0,
+                      icc_slope = 0.05,
+                      var_ratio = 0.02,
+                      partially_nested = TRUE,
+                      cohend = -0.8)
+
+p
+
+## ------------------------------------------------------------------------
+p1 <- study_parameters(n1 = 11,
+                      n2 = 5,
+                      n3 = 5,
+                      icc_pre_subject = 0.5,
+                      icc_pre_cluster = 0,
+                      icc_slope = 0.05,
+                      var_ratio = 0.02,
+                      partially_nested = TRUE,
+                      cohend = -0.8)
+
+p2 <- study_parameters(n1 = 11,
+                      n2 = per_treatment(control = 50, 
+                                         treatment = 5),
+                      n3 = per_treatment(control = 1,
+                                         treatment = 5),
+                      icc_pre_subject = 0.5,
+                      icc_pre_cluster = 0,
+                      icc_slope = 0.05,
+                      var_ratio = 0.02,
+                      partially_nested = TRUE,
+                      cohend = -0.8)
+
+p1
+p2
+
+## ------------------------------------------------------------------------
+p <- study_parameters(n1 = 11,
+                      n2 = 10,
+                      n3 = 5,
+                      icc_pre_subject = 0.5,
+                      icc_pre_cluster = 0, 
+                      var_ratio = 0.02,
+                      icc_slope = 0.05,
+                      dropout = dropout_weibull(proportion = 0.3, 
+                                                rate = 1/2),
+                      fixed_slope = -0.5/10,
+                      cohend = -0.5)
+
+p
+
+## ---- fig.width=8--------------------------------------------------------
+plot(p)
+
+## ---- message = FALSE----------------------------------------------------
+get_power(p)
+
+## ---- fig.width=4--------------------------------------------------------
+d <- per_treatment(control = dropout_weibull(proportion = 0.3, 
+                                                rate = 1/2),
+                   treatment = dropout_weibull(proportion = 0.5, 
+                                                rate = 2))
+
+p2 <- study_parameters(n1 = 11,
+                      n2 = 10,
+                      n3 = 5,
+                      icc_pre_subject = 0.5,
+                      icc_pre_cluster = 0, 
+                      var_ratio = 0.02,
+                      icc_slope = 0.05,
+                      dropout = d,
+                      fixed_slope = -0.5/10,
+                      cohend = -0.5)
+
+plot(p2, plot = 2)
+
+## ---- message = FALSE----------------------------------------------------
+p2 <- study_parameters(n1 = 11,
+                      n2 = c(5, 10, 15, 20, 30),
+                      n3 = 5,
+                      icc_pre_subject = 0.5,
+                      icc_pre_cluster = 0, 
+                      var_ratio = 0.02,
+                      icc_slope = 0.05,
+                      dropout = dropout_weibull(proportion = 0.3, 
+                                                rate = 1/2),
+                      fixed_slope = -0.5/10,
+                      cohend = -0.5)
+get_DEFT(p2)
+
