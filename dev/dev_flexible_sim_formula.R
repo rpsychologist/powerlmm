fb1ecda0e9c5ea1e4ec262e4b43c013a8384622a
Kristoffer Magnusson <1629860+rpsychologist@users.noreply.github.com>
2018-04-20 10:30:09 +0200

    dev folder

---
+++ dev/dev_flexible_sim_formula.R
@@ -0,0 +1,40 @@
+## New formula syntax
+
+list(list("label" = "y ~ time + (1 | cluster)",
+     data_transform = function(),
+     package = "lme4"
+     )
+     )
+
+# or
+list("label1" = list(formula  ="y ~ time + (1 | cluster)",
+                    data_transform = function(),
+                    package = "lme4"
+                    ),
+     "label2" = list(formula  ="y ~ pre + treatment + (1 | cluster)",
+                     data_transform = function(),
+                     package = "lme4"
+                     )
+     )
+
+## backward compat
+## must work with
+f <- list("correct" = "y ~ treatment * time",
+          "wrong" = "y ~ treatment * time + (1 + time | subject)")
+
+## better to use helper functions; transparent arguments
+list("label1" = sim_formula(formula  ="y ~ time + (1 | cluster)",
+                     data_transform = function(),
+                     package = "lme4"
+),
+"label2" = sim_formula(formula  ="y ~ pre + treatment + (1 | cluster)",
+                data_transform = function(),
+                package = "lme4"
+                )
+)
+
+# advantage of also using helper instead of outer list()
+# custom print method for formula object
+
+# need seperate func to combine sim_formula
+# compare_sim_formulas(sim_formula, sim_formula)
