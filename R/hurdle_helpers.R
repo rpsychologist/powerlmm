b8435b329068b1e897e1e03387c7855de1471322
Kristoffer Magnusson <1629860+rpsychologist@users.noreply.github.com>
2018-09-21 15:30:43 +0200

    dummy functions

---
+++ R/hurdle_helpers.R
@@ -0,0 +1,26 @@
+
+get_ICC_pre_subjects.plcp_hurdle <- function(object, ...) {
+    numeric(0)
+}
+get_ICC_pre_subjects.plcp_multi_hurdle <- function(object, ...) {
+    numeric(nrow(object))
+}
+get_ICC_pre_clusters.plcp_hurdle <- function(object, ...) {
+    numeric(0)
+}
+get_ICC_pre_clusters.plcp_multi_hurdle <- function(object, ...) {
+    numeric(nrow(object))
+}
+
+get_ICC_slope.plcp_hurdle  <- function(object, ...) {
+    numeric(0)
+}
+get_ICC_slope.plcp_multi_hurdle <- function(object, ...) {
+    numeric(nrow(object))
+}
+get_var_ratio.plcp_hurdle  <- function(object, ...) {
+    numeric(0)
+}
+get_var_ratio.plcp_multi_hurdle <- function(object, ...) {
+    numeric(nrow(object))
+}
