3c9223019bfab365f389e5764736bd089c34562d
Kristoffer Magnusson <1629860+rpsychologist@users.noreply.github.com>
2018-09-26 20:46:26 +0200

    dev

---
+++ dev/dev_plot_hurdle_CTP.R
@@ -0,0 +1,61 @@
+p1 <- study_parameters(
+    design = des,
+    n1 = 3,
+    n2 = 20,
+    fixed_intercept = log(30), # median(Y > 0)
+    fixed_hu_intercept = qlogis(0.8), # prop == 0
+    fixed_slope = log(0.99),
+    fixed_hu_slope = log(1),
+    sd_hu_intercept = 3,
+    sd_hu_slope = 0.2,
+    sd_intercept = 2,
+    sd_slope = 0.05,
+    cor_intercept_slope = -0.15,
+    cor_intercept_hu_intercept = -0.66,
+    cor_intercept_hu_slope = 0.2,
+    cor_slope_hu_intercept = -0.1,
+    cor_slope_hu_slope = -0.1,
+    cor_hu_intercept_hu_slope = 0.15,
+    shape = 1.6,
+    RR_cont = 0.8,
+    OR_hu = 2,
+    marginal = FALSE,
+    family = "gamma")
+
+
+m <- marginalize(p, R = 1e5)
+m1 <- marginalize(p1, R = 1e5)
+
+
+# time
+## overall
+plot_hurdle_time(m$y_overall)
+plot_hurdle_time(m$y_positive) + scale_y_log10()
+plot_hurdle_time(m$hu_prob)
+
+
+# Diff
+plot_hurdle_diff(m)
+plot_hurdle_diff(m, hu = TRUE)
+
+
+
+
+
+
+plot_hurdle_probs(m)
+
+
+
+ggplot(tmp, aes(percentile, p, color = percentile == 0.5)) +
+    geom_point() +
+    geom_segment(aes(y = 0, yend = OR, xend = percentile)) +
+    geom_hline(yintercept = 0, linetype = "dotted") +
+    geom_hline(yintercept = m$post_hu[m$post_hu$var == "marg_OR", "est"]) +
+    theme_minimal()
+
+
+
+#
+x <- seq(-3, 3, length.out = 100)
+plot(x, plogis(x+1) - plogis(x+1))
---
+++ dev/dev_plot_hurdle_MTP.R
@@ -0,0 +1,60 @@
+p1 <- study_parameters(
+    design = des,
+    n1 = 3,
+    n2 = 20,
+    fixed_intercept = log(300), # median(Y > 0)
+    fixed_hu_intercept = qlogis(0.5), # prop == 0
+    fixed_slope = log(0.99),
+    fixed_hu_slope = log(1),
+    sd_hu_intercept = 0.5,
+    sd_hu_slope = 0,
+    sd_intercept = 3,
+    sd_slope = 0.1,
+    cor_intercept_slope = -0.15,
+    cor_intercept_hu_intercept = -0.66,
+    cor_intercept_hu_slope = 0.2,
+    cor_slope_hu_intercept = -0.1,
+    cor_slope_hu_slope = -0.1,
+    cor_hu_intercept_hu_slope = 0.15,
+    shape = 1.6,
+    RR_cont = 0.8,
+    OR_hu = 2,
+    marginal = FALSE,
+    family = "gamma")
+
+m1 <- marginalize(p1, R = 1e5)
+
+
+# time
+## overall
+plot_hurdle_time(m1$y_overall) + scale_y_log10()
+plot_hurdle_time(m1$y_positive) + scale_y_log10()
+plot_hurdle_time(m1$hu_prob)
+
+
+# Diff
+plot_hurdle_diff(m1)
+plot_hurdle_diff(m1, hu = TRUE)
+
+
+plot_hurdle_probs(m1)
+
+
+
+tmp <- m1$mu_overall_vec
+ps <- seq(0.00000000001, 0.99999999, length.out = 100)
+y <- unlist(lapply(ps, function(p) quantile(tmp[[6]], p)/quantile(tmp[[3]], p)))
+
+
+plot(ps, y)
+
+median(tmp[[6]])/median(tmp[[3]])
+
+mean(tmp[[6]]) - mean(tmp[[3]])
+mean(tmp[[6]]) / mean(tmp[[3]])
+
+
+
+quantile(tmp[[6]], 0.78)/quantile(tmp[[3]], 0.78)
+
+plot(density(tmp[[6]]/tmp[[3]]))
