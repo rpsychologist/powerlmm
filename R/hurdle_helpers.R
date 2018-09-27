c73bbe542c50b8c2fdb396ff56a698779b74db3b
Kristoffer Magnusson <1629860+rpsychologist@users.noreply.github.com>
2018-09-27 18:58:26 +0200

    improved legend

--- R/hurdle_helpers.R
+++ R/hurdle_helpers.R
@@ -96,11 +96,15 @@ plot_hurdle_time <- function(x) {
     tmp$width <- factor(tmp$width,
                         levels = rev(c(0.5, 0.8, 0.95, 0.99)))
 
-    ggplot(x, aes(time, mean, color = treatment, group = treatment)) +
-        geom_ribbon(data = tmp, aes(ymin = min, ymax = max, y = NULL, x= time, group = width, fill = width, color = NULL), alpha = 0.75) +
-        geom_line(linetype = "dotted", size = 1, color = "red") +
-        geom_line(aes(y = Q50), size = 1, color = "red") +
+    ggplot(x, aes(time, mean, group = treatment)) +
+        geom_ribbon(data = tmp, aes(ymin = min, ymax = max, y = NULL, x = time, group = width, fill = width), alpha = 0.75) +
+        geom_line(aes(color = "mean", linetype = "mean", fill = NULL), size = 1) +
+        geom_line(aes(y = Q50, color = "median", linetype = "median", fill = NULL), size = 1) +
         geom_point(aes(y = Q50), color = "red") +
+        scale_color_manual(values = c("median" = "red", "mean" = "red")) +
+        scale_linetype_manual(values = c("median" = "solid", "mean" = "dotted")) +
+        labs(linetype = "", color = "") +
+        guides(color = guide_legend(override.aes = list(fill = NA))) +
         facet_wrap(~treatment, ncol = 2) +
         scale_fill_brewer() +
         theme_minimal()
