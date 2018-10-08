69aadcf0278f3394e5387f23b634710c69e12f5d
Kristoffer Magnusson <1629860+rpsychologist@users.noreply.github.com>
2018-10-08 20:33:52 +0200

    compare to LMM

--- dev/dev_sim_poisson.R
+++ dev/dev_sim_poisson.R
@@ -5,6 +5,8 @@ p <- study_parameters(design = des,
                       n1 = 11,
                       n2 = 25,
                       icc_pre_subject = 0.5,
+                      fixed_intercept = log(5),
+                      fixed_slope = log(0.95),
                       var_ratio = 0.02,
                       effect_size = log(1),
                       sigma_error = 1)
@@ -22,6 +24,14 @@ res <- simulate(p, nsim = 500, cores = 16)
 
 summary(res)
 
+res2 <- simulate(p, nsim = 16, cores = 16, CI = TRUE)
+
+
+
+# fit LMM instead
+f <- sim_formula("y ~ time*treatment + (1 + time | subject)")
+res3 <- simulate(p, formula = f, nsim = 500, cores = 16)
+summary(res3)
 
 
 # gaussian
